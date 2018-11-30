f6c0b6489ee25e39c32d4df51489d70507d482d3
Javier Garcia <info@javiergarcia-design.com>
2018-11-30 10:16:06 +0100

    Added boards endpoint with lists and cards and lists and cards counter

--- server.js
+++ server.js
@@ -16,14 +16,47 @@ const app = express();
 app.use(cors());
 
 app.get("/boards", (req, res) => {
-  setTimeout(() => res.json(boards), Math.floor(Math.random() * 5000));
+  const responseBoards = boards.map(board => {
+    board.listsCounter = lists.reduce((counter, currentList) => {
+      if (currentList.board_id === board._id) return counter + 1;
+
+      return counter;
+    }, 0);
+
+    let cardsCounter = 0;
+
+    lists.forEach(list => {
+      if (list.board_id === board._id) {
+        cardsCounter += cards.reduce((counter, currentCard) => {
+          if (currentCard.list_id === list._id) return cardsCounter + 1;
+
+          return counter;
+        });
+      }
+    }, 0);
+
+    board.cardsCounter = cardsCounter;
+
+    return board;
+  });
+
+  setTimeout(() => res.json(responseBoards), Math.floor(Math.random() * 5000));
 });
 
 app.get("/boards/:id", (req, res) => {
   const board = boards.find(item => item._id === req.params.id);
   if (board) {
-    console.log(board.title);
-    setTimeout(() => res.json(board), Math.floor(Math.random() * 5000));
+    const responseBoard = Object.assign({}, board);
+    responseBoard.lists = lists.filter(list => {
+      if (list.board_id === board._id) {
+        list.cards = cards.filter(card => card.list_id === list._id);
+
+        return list;
+      }
+    });
+
+    console.log(responseBoard.title);
+    setTimeout(() => res.json(responseBoard), Math.floor(Math.random() * 5000));
   } else {
     console.log(404, req.params.id);
     res.status(404).json({ error: "board not found" });
@@ -50,5 +83,5 @@ app.get("/cards/:id", (req, res) => {
   }
 });
 
-console.log(`Starting server on port 3000`);
-app.listen(3000);
+console.log(`Starting server on port 3001`);
+app.listen(3001);
