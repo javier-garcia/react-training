ec51a15a29a754eca3f6ab29f97495bce57941e1
jgarciamartin <javier.garciamartin@cegedim.com>
2018-12-05 09:05:55 +0100

    added mocked boards data for the not fetching version

---
+++ src/boardsData.js
@@ -0,0 +1,105 @@
+const boardsData = [
+  {
+    _id: "a5d9707e-e74e-4e4a-92c6-0872c5e1945d",
+    title: "Board 1",
+    description: "Description of the board 1",
+    lists: [
+      {
+        _id: "ac3e58a9-e7c1-4516-8f49-52fb06f8b9e7",
+        title: "List 1",
+        board_id: "a5d9707e-e74e-4e4a-92c6-0872c5e1945d",
+        cards: [
+          {
+            _id: "0d2d8d44-842c-4d64-a298-b67f38d791e2",
+            title: "Card 1",
+            excerpt: "This is the excerpt for card 1",
+            content:
+              "Dillinger is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n# New Features!\n\n- Import a HTML file and watch it magically convert to Markdown\n- Drag and drop images (requires your Dropbox account be linked)\n",
+            list_id: "ac3e58a9-e7c1-4516-8f49-52fb06f8b9e7"
+          },
+          {
+            _id: "fa73c31b-33e9-4a7b-b0ac-9046b035906f",
+            title: "Card 2",
+            excerpt: "This is the excerpt for card 2",
+            content:
+              "Dillinger is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n# New Features!\n\n- Import a HTML file and watch it magically convert to Markdown\n- Drag and drop images (requires your Dropbox account be linked)\n",
+            list_id: "ac3e58a9-e7c1-4516-8f49-52fb06f8b9e7"
+          }
+        ]
+      },
+      {
+        _id: "20086f09-2a5a-4d30-89b9-ad09e2c5139d",
+        title: "List 2",
+        board_id: "a5d9707e-e74e-4e4a-92c6-0872c5e1945d",
+        cards: [
+          {
+            _id: "c11344ef-2846-4509-a7df-469141704aef",
+            title: "Card 3",
+            excerpt: "This is the excerpt for card 3",
+            content:
+              "Dillinger is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n# New Features!\n\n- Import a HTML file and watch it magically convert to Markdown\n- Drag and drop images (requires your Dropbox account be linked)\n",
+            list_id: "20086f09-2a5a-4d30-89b9-ad09e2c5139d"
+          },
+          {
+            _id: "2f8c904f-9a7b-4a9e-aa36-2fedbcbd45e3",
+            title: "Card 4",
+            excerpt: "This is the excerpt for card 4",
+            content:
+              "Dillinger is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n# New Features!\n\n- Import a HTML file and watch it magically convert to Markdown\n- Drag and drop images (requires your Dropbox account be linked)\n",
+            list_id: "20086f09-2a5a-4d30-89b9-ad09e2c5139d"
+          },
+          {
+            _id: "7f8de31d-de06-4c65-bcc1-998a363fc1f5",
+            title: "Card 5",
+            excerpt: "This is the excerpt for card 5",
+            content:
+              "Dillinger is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n# New Features!\n\n- Import a HTML file and watch it magically convert to Markdown\n- Drag and drop images (requires your Dropbox account be linked)\n",
+            list_id: "20086f09-2a5a-4d30-89b9-ad09e2c5139d"
+          }
+        ]
+      },
+      {
+        _id: "6c6177fd-3071-4605-b0b5-7e1fce2de361",
+        title: "List 3",
+        board_id: "a5d9707e-e74e-4e4a-92c6-0872c5e1945d",
+        cards: [
+          {
+            _id: "ccd8f4ce-a934-412a-bcf8-48f088bdd6d9",
+            title: "Card 6",
+            excerpt: "This is the excerpt for card 6",
+            content:
+              "Dillinger is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n# New Features!\n\n- Import a HTML file and watch it magically convert to Markdown\n- Drag and drop images (requires your Dropbox account be linked)\n",
+            list_id: "6c6177fd-3071-4605-b0b5-7e1fce2de361"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    _id: "509eab86-a23c-4030-94c1-b05192744d4c",
+    title: "Board 2",
+    description: "Description of the board 2",
+    lists: [
+      {
+        _id: "e67955aa-7eb3-455d-ae42-847320f4aa10",
+        title: "List 1",
+        board_id: "509eab86-a23c-4030-94c1-b05192744d4c",
+        cards: []
+      }
+    ]
+  },
+  {
+    _id: "7c6d7fa1-5442-4b51-aa9d-b4d9af5abf2d",
+    title: "Board 3",
+    description: "Description of the board 3",
+    lists: []
+  },
+  {
+    _id: "0c55d7f6-dc35-43ce-8a02-d6acc57603a8",
+    title: "Board 4",
+    description: "Description of the board 4",
+    lists: []
+  }
+];
+
+export default boardsData;
